51b1e7a00bbccd5614d2bb32d7060cfd11566934
aldrich201 <angaldrich31@gmail.com>
2020-05-31 16:32:21 +1000

    edited server

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ var commentRoutes = require("./routes/comments"),
 
 //seedDB(); //seed database
 //app config
+// mongoose.connect("mongodb://localhost/yelp_camp_v10", {
+// 	useNewUrlParser: true, 
+// 	useUnifiedTopology: true, 
+// 	useFindAndModify: false
+// }).then(() => {
+// 	console.log("Connected to Database");
+// }).catch(err => {
+// 	console.log("ERROR:", err.message);
 mongoose.connect("mongodb+srv://aldrich201:Hongtienchai12@cluster0-mk411.mongodb.net/test?retryWrites=true&w=majority", {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true, 
@@ -61,7 +69,7 @@ app.use("/campgrounds", campgroundRoutes);
 
 var port = process.env.PORT || 3001;
 app.listen(port, function () {
-  console.log("CONNECTED");
+  console.log("Connected to Server");
 });
 
 //<%- include("partials/header") %>
